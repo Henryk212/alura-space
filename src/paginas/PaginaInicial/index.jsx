3deb6bf8f806d61dcd3f2d922673fcdd136bf4e3
Henryk212 <111775517+Henryk212@users.noreply.github.com>
2023-06-03 11:42:43 -0300

    Galeria

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,23 +1,29 @@
+import banner from "./banner.png";
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
-import Banner from "../../componentes/Banner";
-import styles from '../PaginaInicial/PaginaInicial.module.scss';
+import styles from "./PaginaInicial.module.scss";
 import Rodape from "../../componentes/Rodape";
+import Galeria from "../../componentes/Galeria";
+import Populares from "../../componentes/Populares";
 
-export default function PaginaInicial(){
-    return ( 
-        <>
-        
-        <Cabecalho/>
-        <main>
-            <section className={styles.principal}>
-               <Menu/>
-               <Banner/>
-            </section>
-        </main>  
-        <Rodape/>  
-        </>
-    ) 
-        
-    
+export default function PaginaInicial() {
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
+      <Rodape />
+    </>
+  );
 }
